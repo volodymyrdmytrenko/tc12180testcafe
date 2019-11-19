87a36e6160c287fa31775c0d0c694efa9a084efb
Volodymyr <volodymyr.dmytrenko@gmail.com>
2019-11-19 15:33:56 +0200

    go on..

--- tc12180.js
+++ tc12180.js
@@ -34,24 +34,27 @@ let main_matrix = [["for_em", "for_mn", "for_hr", "role", "any", "oi", "all", "d
                        ["false", "true", "true", "hr", "can", "can", "can", "skip", "skip"],  // 31
                        ["false", "true", "true", "hrmn", "can", "can", "can", "can", "can"]];  // 32
 
-let users = {'em': 'Martha.Robinson@email.com',            // employee
-            'em1': 'Jenny.Thompson@email.com',             // employee1
-            'oi': 'Gerry.Harris@email.com',                // OI user
-            'mn': 'Dermot.Jackson@email.com',              // manager
-            'sub': 'David.Wilson@email.com',               // sub
-            'hr': 'alison.johnson@email.com',              // HR
-            'hrmn': 'Jamie.Duggan@email.com',              // HR+manager
-            'dir': 'Tina.Delaney@email.com',               // HR+manager direct
-            'admin': 'Barry.Deegan@email.com'};            // admin
+let users = {em : 'Martha.Robinson@email.com',            // employee
+            any : 'Jenny.Thompson@email.com',             // employee1
+            oi : 'Gerry.Harris@email.com',                // OI user
+            mn : 'Dermot.Jackson@email.com',              // manager
+            sub : 'David.Wilson@email.com',               // sub
+            hr : 'alison.johnson@email.com',              // HR
+            hrmn : 'Jamie.Duggan@email.com',              // HR+manager
+            dir : 'Tina.Delaney@email.com',               // HR+manager direct
+            admin : 'Barry.Deegan@email.com'};            // admin
 
 
 let url = 'http://stage.mytandem.eu';
 let db = 'e2e';
 let pw = 'pass';
-let usr = 'Martha.Robinson@email.com';
+// let usr = 'Martha.Robinson@email.com';
+let testNumber = 0;
+let testName = '';
+let destination = '';
 
 
-fixture `001tc12180`
+fixture `tc12180`
     .page `${url}`;
 
 class LoginPage {
@@ -61,6 +64,7 @@ class LoginPage {
         this.userName              = Selector('#e2e-username');
         this.password              = Selector('#e2e-password');
         this.loginButton           = Selector('#e2e-login-button');
+        this.confirm               = Selector('#e2e-yes-button')
     }
 };
 
@@ -68,59 +72,135 @@ class AdminPage {
     constructor () {
         this.administration      = Selector('#e2e-system-administration');
         this.profile             = Selector('#e2e-user-profile');
+        this.logout              = Selector('#e2e-dropdown-profile-logout')
         this.goals               = Selector('#e2e-manage-goals');
         this.AssignGoalUsr       = Selector('#GoalsSettings_EnableAllowEmployeeToAssignToEveryone')
         this.AssignGoalMng       = Selector('#GoalsSettings_EnableAllowManagerToAssignToEveryone');
         this.AssignGoalHR        = Selector('#GoalsSettings_EnableAllowHRToAssignToEveryone');
-        this.confirm             = Selector('#main-content > div:nth-child(2) > div.sc-bxivhb.gxUcVt > div.givefb-footer.pt-3.flex-center-end > button > span');
-        
+        this.confirm             = Selector('.tandem-btn').withText('Save');
     }
 };
 
-function fLogin(db_t,usr_t,pw_t){
-  const page = new LoginPage();
-  test('tstLogin', async t => {
-      await t
-          .typeText(page.dataBase, db_t)
-          .click(page.continueButton)
-          .typeText(page.userName, usr_t)
-          .typeText(page.password, pw_t)
-          .click(page.loginButton)
-          .expect(Selector('#e2e-user-profile').exists).ok();
-  });
+class AssignGoalPage {
+    constructor () {
+        this.assignGoal         = Selector('#e2e-assign-goal');
+        this.sendTo             = Selector('#Sendto\:');
+        this.foundItem          = Selector('#found-item');
+        this.type               = Selector('#e2e-set-goal-type');
+        this.typeSelect         = Selector('.tandem-multiselect__menu-list').withText('Development');
+        this.datePriority       = Selector('#newPriorityDueDate');
+        this.title              = Selector('.public-DraftEditorPlaceholder-inner').nth(0);
+        this.titleContent       = Selector('.public-DraftEditor-content').nth(0);
+        this.send               = Selector('.tandem-btn').withText('Send');
+        this.confirm            = Selector('#e2e-primary-button');
+    }
 };
 
-function fSettings(em,mn,hr) {
-  const page = new AdminPage();
-  let switched = false;
-  test('tstSetting', async t => {
-      await t
-        .click(page.administration)
-        .click(page.goals)
-        if (page.AssignGoalUsr.getAttribute('data-value').notEql(em)) {
-            t.click(page.AssignGoalUsr);
-            switched = true;
-        };
-        if (page.AssignGoalMng.getAttribute('data-value').notEql(mn)) {
-            t.click(page.AssignGoalUsr)
-            switched = true;
-        };
-        if (page.AssignGoalUsr.getAttribute('data-value').notEql(hr)) {
-            t.click(page.AssignGoalUsr)
-            switched = true;
-        };
-        if (switched) {
-            t.click(page.confirm)
-        };
+const login_page = new LoginPage();
+const admin_page = new AdminPage();
+const assignGoal_page = new AdminPage();
 
-    });
-};
+for (let i=1; i<=32; i++ ) {
+    for (let j=4; j<=8; j++) {
+        if (main_matrix[i][j] == 'skip') { break }
+        // Test name create
+        testName = ('00' + ++testNumber).slice(-3) + '_'
+        if (main_matrix[i][0] == 'true') {
+            testName = testName + 'on_'
+        } else {
+            testName = testName + 'off_'
+        };
+        if (main_matrix[i][1] == 'true') {
+            testName = testName + 'on_'
+        } else {
+            testName = testName + 'off_'
+        };
+        if (main_matrix[i][2] == 'true') {
+            testName = testName + 'on_'
+        } else {
+            testName = testName + 'off_'
+        };
+        testName = testName + main_matrix[i][3] + '_' + main_matrix[0][j] + '_' + main_matrix[i][j];
+        console.log(testName);
 
-fLogin(db,usr,pw);
+        // Start test
+        test(`tc12180_${testName}`, async t => {
+            await t
+            // Login as admin
+            .typeText(login_page.dataBase, db)
+            .click(login_page.continueButton)
+            .typeText(login_page.userName, users['admin'])
+            .typeText(login_page.password, pw)
+            .click(login_page.loginButton)
+            .expect(Selector('#e2e-user-profile').exists).ok()
+            
+            // Go Administration -> Goal
+            .click(admin_page.administration)
+            .click(admin_page.goals)
+            const em_page = await admin_page.AssignGoalUsr.getAttribute('data-value')
+            const mn_page = await admin_page.AssignGoalMng.getAttribute('data-value')
+            const hr_page = await admin_page.AssignGoalHR.getAttribute('data-value')
+            
+            // Settings fo Goals
+            let switched = false
+            if ( em_page != main_matrix[i][0]) {
+                await t.click(admin_page.AssignGoalUsr);
+                switched = true;
+            };
+            if (mn_page != main_matrix[i][1]) {
+                await t.click(admin_page.AssignGoalUsr)
+                switched = true;
+            };
+            if (hr_page != main_matrix[i][2]) {
+                await t.click(admin_page.AssignGoalHR)
+                switched = true;
+            };
+            if (switched) {
+                await t.click(admin_page.confirm)
+            };
 
-for (i=1; i<=32; i++ ) {
-    fSettings(main_matrix[i][0], main_matrix[i][0], main_matrix[i][0]);
-    for (j=4; j<=8; j++) {
-        
-    };
+            // Admin logout
+            await t
+                .click(admin_page.profile)
+                .click(admin_page.logout)
+                .click(login_page.confirm)
+
+            // Login as initial user
+            await t
+                // .typeText(login_page.dataBase, db)
+                // .click(login_page.continueButton)
+                .typeText(login_page.userName, users[main_matrix[i][3]])
+                .typeText(login_page.password, pw)
+                .click(login_page.loginButton)
+                .expect(Selector('#e2e-user-profile').exists).ok()
+            
+            // If user have not menu Set Goal 
+            if (main_matrix[i][j] == 'no') {
+                await t
+                    .expect(admin_page.setGoal.exists).notOk()
+
+            // If user can assign Goal
+            } else if (main_matrix[i][j] == 'can') {
+                if (main_matrix[0][j] == 'any') {
+                    destination = users['any'];
+                    // console.log(destination);
+                    await t
+                        .click(assignGoal_page.assignGoal)
+                        // set destination
+                        .typeText(assignGoal_page.sendTo, destination)
+                        .click(assignGoal_page.foundItem)
+                        // set date
+                        .typeText(assignGoal_page.datePriority, '31/12/2030')
+                        // set Goal type
+                        .click(assignGoal_page.type)
+                        .click(assignGoal_page.typeSelect)
+                        // set Title
+                        .click(assignGoal_page.title)
+                        .typeText(assignGoal_page.titleContent, testName)
+                        .click(assignGoal_page.send)
+                        .click(assignGoal_page.confirm)
+                }
+            }
+        })
+    }
 }
